0602cf1451cffcd4e0578559182b38826e8af42f
Favour Mba <mbafavour2020@gmail.com>
2023-08-14 02:47:04 -0700

    Using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point starts here
+ *
+ * Return: Always zero (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
