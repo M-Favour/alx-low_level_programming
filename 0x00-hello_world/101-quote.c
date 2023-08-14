b5bb6aa65909d1b9bcc541bf62de73cdd3ce7cdd
Favour Mba <mbafavour2020@gmail.com>
2023-08-14 07:33:51 -0700

    No printf() or puts()

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Your code starts here
+ *
+ * Return: Always one (1)
+ */
+int main(void)
+{
+	/*intialises myStr*/
+	char myStr = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, myStr);
+	return (1);
+}
