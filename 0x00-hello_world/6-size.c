93904c076a35c9149bbc0dcf9feedb9b8b6e9df5
Favour Mba <mbafavour2020@gmail.com>
2023-08-14 03:14:25 -0700

    Using sizeof()

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Your code starts here
+ *
+ * Return: Always zero (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d(s)\n", sizeof(char));
+	printf("Size of an int: %d(s)\n", sizeof(int));
+	printf("Size of a long int: %d(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d(s)\n", sizeof(long long int));
+	printf("Size of a float: %d(s)\n", sizeof(float));
+	
+	return (0);
+}
