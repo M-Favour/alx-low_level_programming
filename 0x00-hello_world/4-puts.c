1df7dc90a0972374bb93a26e6466af147d4150f0
Favour Mba <mbafavour2020@gmail.com>
2023-08-14 02:34:00 -0700

    Using puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point starts here
+ *
+ * Return: Always zero (Success)
+ */
+int main(void)
+{
+	/*string is initialized*/
+	char myStr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(myStr);/*prints the string*/
+	return (0);
+}
