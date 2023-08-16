25bae815b87b755472be9c6a96f245629896e436
Favour Mba <mbafavour2020@gmail.com>
2023-08-16 06:49:59 -0700

    New Commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Your code starts here
+ *
+ * Return: Always zero (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* your code goes there */
+	if (n > 0)
+		putchar(n + "is positive");
+	else if (n < 0)
+		putchar(n + "is negative");
+	else
+		putchar(n + "is zero");
+
+	return (0);
+}
