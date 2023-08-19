94f49ee44d4bf8c75e5808ddeaa62199360598a6
Favour Mba <mbafavour2020@gmail.com>
2023-08-19 13:51:42 -0700

    Print lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Your code starts here
+ *
+ * Return: Always zero (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 65; i >= 90; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
