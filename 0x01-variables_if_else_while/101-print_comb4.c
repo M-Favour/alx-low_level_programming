ad468e43266bd9844c9acffd68832f4a5f63d4dd
Favour Mba <mbafavour2020@gmail.com>
2023-08-20 10:36:39 -0700

    Print three-digit combos

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,11 +18,15 @@ int main(void)
 				putchar(i + '0');
 				putchar(j + '0');
 				putchar(k + '0');
-				
+
 				if (i != 7 || j != 8 || k != 9)
 				{
 					putchar(',');
 					putchar(' ');
+				}
+				else if (i == j || j == k || i == k)
+				{
+					continue;
 				}
 				else
 				{
