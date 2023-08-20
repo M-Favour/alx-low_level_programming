4cd6ee1f72023007c4634a88b7b210dbfbce43e3
Favour Mba <mbafavour2020@gmail.com>
2023-08-20 07:27:55 -0700

    Print z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Your code starts here
+ *
+ * Return: Always zero (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i <= 97; i--)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
