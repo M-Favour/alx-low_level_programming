4f961662b8548c2f068003704bc7f700b0871352
Favour Mba <mbafavour2020@gmail.com>
2023-08-20 06:50:39 -0700

    Print 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Your code starts here
+ *
+ * Return: Always zero (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i + '0');
+	putchar('\n');
+
+	return (0);
+}
