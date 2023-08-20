b0f38dba5fe02a66751bfb4bb676862f6a6bacd1
Favour Mba <mbafavour2020@gmail.com>
2023-08-20 10:24:30 -0700

    Print three-digit combos

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Your code starts here
+ *
+ * Return: Always zero
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = 0; j <= 8; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				else
+				{
+					putchar('\n');
+				}
+			}
+		}
+	}
+
+	return (0);
+}
