0fbdc58c9906cafc2800722538d462398837ca3c
Favour Mba <mbafavour2020@gmail.com>
2023-08-24 07:59:18 -0700

    Print a-z ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints a-z ten times
+ *
+ * Return:returns void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 1; i <= 10; i++)
+	{
+		_putchar(j);
+		_putchar('\n');
+	}
