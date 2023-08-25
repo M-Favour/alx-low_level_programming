09d069fe54d44a3c7cd9b3524e9f3ea6a4d4be1e
Favour Mba <mbafavour2020@gmail.com>
2023-08-25 16:22:25 -0700

    Checks for lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - Checks if a character is in upper or lowercase
+ *
+ * @c: the character to be checked
+ *
+ * Return: Always zero
+ */
+
+int _islower(int c)
+{
+	int c = 65;
+
+	if (c <= 90)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
